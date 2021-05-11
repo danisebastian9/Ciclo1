3cbecd26a829480cbe7b03262163dadb7c9979e2
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-11 17:08:56 -0500

    Python basics 1

---
+++ 11-05.py
@@ -0,0 +1,28 @@
+'''
+print ("Hello World ") # Mostrar mensajes y variables por pantalla
+def # Define una funcion 
+if #  Indica condiciones
+else # Cuando una condicion es falsa
+elif # Anidar condiciones cuando no se cumple alguna. 
+return # retornar un valor desde una funcion. 
+True # Verdadero, 
+False # Falso
+for # Implementa una estructura ciclica 
+input # Ingresar datos desde el teclado. 
+'''
+
+# Identificadores = Nombres que le damos a las variables. 
+
+print ("Hello team")
+nombreEstudiante = "Daniel" # string = cadena de caracteres
+edad = 26 # int = numero entero
+estatura = 1.75 # float = numeros que tienen decimales
+mujer = False # bool = valores True o False
+
+suma = 5 + 4
+
+print(type(mujer)) # Mostrar el tipo de dato asignado a la variable
+
+
+
+
