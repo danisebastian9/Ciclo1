b873ae093334aa0dec2854bae16c12acd435fa90
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-11 17:39:54 -0500

    Python basics class finished

--- 11-05.py
+++ 11-05.py
@@ -13,6 +13,8 @@ input # Ingresar datos desde el teclado.
 
 # Identificadores = Nombres que le damos a las variables. 
 
+import math
+
 print ("Hello team")
 nombreEstudiante = "Daniel" # string = cadena de caracteres
 edad = 26 # int = numero entero
@@ -22,6 +24,33 @@ mujer = False # bool = valores True o False
 suma = 5 + 4
 
 print(type(mujer)) # Mostrar el tipo de dato asignado a la variable
+
+#Operadores aritmeticos: 
+a = 5
+b = 3 
+suma = a + b
+print("La suma de", a, "+", b, " = ", suma)
+resta = a - b
+print("La resta de", a, "-", b, " = ", resta)
+multiplicacion = a * b
+print("La multiplicacion de", a, "*", b, " = ", multiplicacion)
+division = a / b 
+print("La division de", a, "/", b, " = ", division)
+divEnt = a // b 
+print("La divison entera de", a, "//", b, " = ", divEnt)
+residuo = a % b 
+print("El modulo de", a, "%", b, " = ", residuo)
+exponente = a ** b 
+print("La potencia de", a, "**", b, " = ", exponente)
+
+prioridad = 3 / a*2
+print(prioridad)
+
+negativo = abs(8)  # Valor absoluto de los numeros dentro de los parentesis.
+print(negativo)
+
+print(math.sqrt(46)) 
+
 
 
 
