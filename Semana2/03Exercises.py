e3d7c49a52dc273b242aac9d68edf376cf0a8309
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-13 16:27:40 -0500

    exercises finished

---
+++ Semana2/03Exercises.py
@@ -0,0 +1,36 @@
+phrase = "La Vida Es Bella"
+
+# Contar cuantos caracteres tiene la cadena "La Vida Es Bella"
+count = len(phrase)
+print(count)
+
+# Reemplazar los espacios de la cadena "La Vida Es Bella" por "______"
+replace = phrase.replace(" ", "______")
+print(replace)
+
+# Imprimir 20 veces la cadena "Hola Hola"
+chain1 = "Hola Hola"
+multiplyChain = chain1 * 20
+print(multiplyChain)
+
+# Buscar la palabra "es" en la cadena " La Vida Es Bella"
+find1 = phrase.find("es")
+print(find1)
+
+# Buscar la palabra "hola" en la cadena "La Vida Es Bella"
+find2 = phrase.find("hola")
+print(find2)
+
+# Imprimir la "La Vida Es Bella" en mayuscula
+print(phrase.upper())
+
+# Imprimir la ultima palabra de la cadena "La Vida Es Bella"
+print(phrase[11:])
+
+# Imprimir la primera palabra de la cadena "La Vida es Bella"
+print(phrase[0:2])
+
+#Imprimir la segunda palabra de la cadena "La Vida es Bella"
+print(phrase[3:7])
+
+
