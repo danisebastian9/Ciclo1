effd3ca78062516c30b9812037f6e55e28f7dd6b
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-13 16:10:05 -0500

    manejo cadena caracteres

--- Semana2/02Relational.py
+++ Semana2/02Relational.py
@@ -27,3 +27,43 @@ año = 2021
 print(dia != "Martes" or mes == "Junio" and año < 2020) # True
 print(not(dia != "Martes" or mes == "Junio" and año < 2020)) # False, not niega la respuesta
 
+# Manejo de cadenas de caracteres. 
+
+cadena1 = "Hola"
+cadena2 = " "
+cadena3 = "Mundo"
+
+frase = cadena1 + cadena2 + cadena3
+print(frase)
+
+repetir = frase * 5
+print(repetir)
+
+print(len(frase))
+
+print(cadena1[0])
+
+print(cadena1[1:3])
+
+print(cadena1[2:])
+
+buscar = frase.find("M")
+print(buscar)
+
+buscar1 = frase.find("es")
+print(buscar1) # -1 no encontro resultado. 
+
+nombre = "MARIA"
+print(nombre.lower())
+
+apellido = "Sanchez"
+print(apellido.upper())
+
+reemplazar = cadena3.replace("d","HOLA")
+print(reemplazar)
+
+reemplazar1 = frase.replace("o", "Hi")
+print(reemplazar1)
+
+
+
