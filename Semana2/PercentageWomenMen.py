b8b64c7922e9a5ecbb8ab43f8600684bad2d19c5
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-12 23:54:46 -0500

    Comments added

--- Semana2/PercentageWomenMen.py
+++ Semana2/PercentageWomenMen.py
@@ -1,6 +1,9 @@
+# This exercise calculates the percentage of Women and Men students present in a course by adding the amount of women and men through the input.
+
 quantW = int(input("Add the amount of Women Students "))
 quantM = int(input("Add the amount of Men Students "))
 totalStudents = quantM + quantW
 percentW = quantW * 100 / totalStudents
 percentM = quantM * 100 / totalStudents
 print("The percentage of Women is ", percentW, '% and the Men percentage is ', percentM, "%")
+
