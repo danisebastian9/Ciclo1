c775674bf6c930766986c8bdeab61b0c58f7a37d
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-15 17:41:07 -0500

    Exercise2

---
+++ Semana3/Exercise2.py
@@ -0,0 +1,7 @@
+'''
+Hacer un algoritmo donde se ingrese dos números y realice una 
+operación matemática de acuerdo al siguiente menú (Debe mostrarse el menú): 
+MENU PRINCIPAL a. SUMA b. RESTA c. MULTIPLICACION d. DIVISION
+'''
+
+
