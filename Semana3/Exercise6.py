8e1b737aa4391ae83d0700df8fc535249ac9a521
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-18 17:42:22 -0500

    Ejercicio6Creado

---
+++ Semana3/Exercise6.py
@@ -0,0 +1,27 @@
+'''
+Una institución educativa estableció un programa para estimular a
+los aprendices con buen rendimiento académico y que consiste en
+lo siguiente:
+Si el promedio es de 9.5 o más y el aprendiz es de secundaria, entonces
+este podrá cursar 55 unidades y se le hará un 25% de descuento.
+Si el promedio es mayor o igual a 9 pero menor que 9.5 y el aprendiz es de
+secundaria, entonces este podrá cursar 50 unidades y se le hará un 10% de
+descuento.
+Si el promedio es mayor que 7 y menor que 9 y el aprendiz es de
+secundaria, este podrá cursar 50 unidades y no tendrá ningún descuento.
+Si el promedio es de 7 o menor, el número de materias reprobadas es de 0
+a 3 y el aprendiz es de secundaria, entonces podrá cursar 45 unidades y no
+tendrá descuento.
+Si el promedio es de 7 o menor, el número de materias reprobadas es de 4
+o más y el aprendiz es de secundaria, entonces podrá cursar 40 unidades y
+no tendrá ningún descuento.
+Si el promedio es mayor o igual a 9.5 y el aprendiz es de profesional,
+entonces podrá cursar 55 unidades y se le hará un 20% de descuento. Sí el
+promedio es menor de 9.5 y el aprendiz es de profesional, entonces podrá
+cursar 55 unidades y no tendrá descuento.
+Obtener el total que tendrá que pagar un aprendiz si la matrícula para
+aprendices de profesional es de $300 por cada cinco unidades y para
+aprendices de secundaria es de $180 por cada cinco unidades.
+'''
+
+
