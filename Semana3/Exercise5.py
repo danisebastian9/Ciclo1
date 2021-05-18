42139e4436eef066840f719e7dc4a92863e56e9e
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-18 17:37:30 -0500

    Ejercicio 5 creado

---
+++ Semana3/Exercise5.py
@@ -0,0 +1,9 @@
+'''
+En una empresa de 1000 trabajadores, se hará un aumento al salario
+de acuerdo al tiempo de servicio, para este aumento se tomará en
+cuenta lo siguiente:
+Tiempo de servicio: de 1 a 5 años Aumento: 10%
+Tiempo de servicio: de 5 a 10 años Aumento: 15%
+Tiempo de servicio: de 10 a 20 años Aumento: 20%
+Tiempo de servicio: de 20 años a más Aumento: 35%
+'''
