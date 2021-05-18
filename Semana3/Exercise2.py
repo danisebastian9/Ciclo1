51aef18e57bee4accd23c25110837f0b42ea9a77
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-17 23:43:20 -0500

    basics created

--- Semana3/Exercise2.py
+++ Semana3/Exercise2.py
@@ -4,4 +4,15 @@ operación matemática de acuerdo al siguiente menú (Debe mostrarse el menú):
 MENU PRINCIPAL a. SUMA b. RESTA c. MULTIPLICACION d. DIVISION
 '''
 
+numero1 = int(input('Ingrese numero 1 para la operación: '))
+numero2 = int(input('Ingrese numero 2 para la operación: '))
+
+operacion = str(input('''
+Seleccione la operacion a realizar, responda A, B, C o D respectivamente:
+A - SUMA
+B - RESTA
+C - MULTIPLICACION
+D - DIVISION
+'''))
+
 
