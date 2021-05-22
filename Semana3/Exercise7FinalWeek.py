fc060f959f96c29d15a67616874eeebe057933b8
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-21 23:25:16 -0500

    ejercicio finalizado

--- Semana3/Exercise7FinalWeek.py
+++ Semana3/Exercise7FinalWeek.py
@@ -31,38 +31,44 @@ ingreso = float(input('Ingrese la cantidad de salarios minimos mensuales de su i
 puntaje = int(input('Ingrese su puntaje del examen de aptitud entre 0 y 100: '))
 
 if edad >= 15 and edad <= 18: 
-    descuentoEdad = 0.25
+    descuentoEdad = 25
 elif edad >= 19 and edad <= 21:
-    descuentoEdad = 0.15
+    descuentoEdad = 15
 elif edad >= 22 and edad <= 25:
-    descuentoEdad = 0.10
+    descuentoEdad = 10
 else: 
     descuentoEdad = 0
 
 if ingreso <= 1: 
-    descuentoIngreso = 0.30
+    descuentoIngreso = 30
 elif ingreso > 1 and ingreso <= 2:
-    descuentoIngreso = 0.20
+    descuentoIngreso = 20
 elif ingreso > 2 and ingreso <= 3:
-    descuentoIngreso = 0.10
+    descuentoIngreso = 10
 elif ingreso > 3 and ingreso <= 4:
-    descuentoIngreso = 0.5
+    descuentoIngreso = 5
 else:
     descuentoIngreso = 0
 
 
 if puntaje >= 96:
-    descuentoPuntaje = 0.45
+    descuentoPuntaje = 45
 elif puntaje >= 91 and puntaje < 96:
-    descuentoPuntaje = 0.40
+    descuentoPuntaje = 40
 elif puntaje >= 86 and puntaje < 91:
-    descuentoPuntaje = 0.35
+    descuentoPuntaje = 35
 elif puntaje >= 80 and puntaje < 86:
-    descuentoPuntaje = 0.30
+    descuentoPuntaje = 30
 else:
     descuentoPuntaje = 0
 
 
-descuentoTotal = descuentoEdad + descuentoIngreso + descuentoPuntaje
+descuentoTotal = descuentoEdad + descuentoIngreso + descuentoPuntaje 
 
+
+print(beneficiario)
+print('El descuento por edad es: %', descuentoEdad)
+print('El descuento por ingreso familiar es: %', descuentoIngreso)
+print('El descuento por puntaje de examen es: %',descuentoPuntaje)
+print('El descuento total sobre el valor de la matricula es: %',descuentoTotal)
 
