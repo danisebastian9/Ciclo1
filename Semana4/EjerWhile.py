5d441dd55e2b6bd0273cadc45583495befbfbeb7
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-24 23:58:52 -0500

    ejercicio completado con errores

--- Semana4/EjerWhile.py
+++ Semana4/EjerWhile.py
@@ -1,4 +1,6 @@
 # Hacer la tabla de multiplicar desde N hasta M 
+n = int(input('Digite limite inferior: '))
+m = int(input('Digite limite superior: '))
 
 while n > m: 
     print('El limite inferior debe ser menor que el superior, vuelva a ingresarlos')
@@ -8,7 +10,7 @@ while n > m:
 contador = 1
 
 while n <= m: 
-    mult = contador * m 
+    mult = contador * n 
     print(n,'X', m, '=', mult)
-
+    contador = contador + 1
 
