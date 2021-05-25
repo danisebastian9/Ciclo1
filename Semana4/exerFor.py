9ac863cf2505b3383b12c463b2b2a738b7f93903
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-25 17:22:29 -0500

    ejercicio completado

--- Semana4/exerFor.py
+++ Semana4/exerFor.py
@@ -1,3 +1,10 @@
 # Realizar un programa que imprima los numeros pares desde
 # x hasta y
 
+x = int(input('Ingrese rango inferior: '))
+y = int(input('Ingrese rango superior: '))
+
+for i in range(x, y):
+    if i % 2 == 0: 
+        print(i)
+         
