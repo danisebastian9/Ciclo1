827a913c83b078de1aa1affa63f39c82a7f4bd9a
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-24 23:55:23 -0500

    basics added

--- Semana4/EjerWhile.py
+++ Semana4/EjerWhile.py
@@ -1,5 +1,14 @@
 # Hacer la tabla de multiplicar desde N hasta M 
 
-n = int(input('Digite limite inferior: '))
-m = int(input('Digite limite superior: '))
+while n > m: 
+    print('El limite inferior debe ser menor que el superior, vuelva a ingresarlos')
+    n = int(input('Digite limite inferior: '))
+    m = int(input('Digite limite superior: '))
+
+contador = 1
+
+while n <= m: 
+    mult = contador * m 
+    print(n,'X', m, '=', mult)
+
 
