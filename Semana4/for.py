18d36bab2a18d052f0052aadfb9e6ae281791076
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-25 17:12:40 -0500

    ejercicios for

---
+++ Semana4/for.py
@@ -0,0 +1,18 @@
+
+# for i in range(5, 20, 5):
+#     print(i)
+    
+# x = 2
+# y = 10 
+# paso = 3
+
+# for i in range(x, y, paso):
+#     print(i)
+
+x = 20
+y = 2 
+paso = -3
+
+for i in range(x, y, paso):
+    print(i)
+
