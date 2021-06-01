7fba69f1bdaec106fa5f1dfb09d9dbefb3bceaec
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-05-31 22:23:06 -0500

    Contexto del ejercicio

---
+++ Semana5/Exer01.py
@@ -0,0 +1,11 @@
+# Ingresar notas de estudiantes hasta que ingrese nota 0.0
+# Decir cual es la nota Minima  
+# Decir cual es la nota Maxima
+# Cuantas son menores a 3, osea las que reprobaron
+# Cuantas son mayores o iguales a 3, osea las que aprobaron
+# Sacar promedio de notas que reprobaron
+# Sacar promedio de notas que aprobaron
+# Notas son de 0 a 5
+# Porcentaje de los que aprobaron 
+# Porcentaje de los que reprobaron. 
+
