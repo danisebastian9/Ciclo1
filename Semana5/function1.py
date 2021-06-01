caec43a411a0e6274c067f3514f203043af8f64c
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-06-01 15:43:20 -0500

    functions01

---
+++ Semana5/function1.py
@@ -0,0 +1,17 @@
+# str()
+# int()
+# float()
+# str.lower()
+# str.upper()
+# print()
+# len('cadena')
+# range(20)
+# input()
+# ord()
+
+#string.join(lista)
+z = '____'
+lista = ['a','b','c','d']
+print(z.join(lista))
+
+
