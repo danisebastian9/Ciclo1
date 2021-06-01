1271541cf29f7d8e2844aa3d0b0b6f0a5b31d293
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-06-01 16:32:49 -0500

    funciones import

--- Semana5/function1.py
+++ Semana5/function1.py
@@ -14,4 +14,67 @@ z = '____'
 lista = ['a','b','c','d']
 print(z.join(lista))
 
+# string.split('_') fragmentar un text de acuerdo a un patron
+texto = 'La_vida_es_bella'
+miLista = list(texto.split('_'))
+print(miLista)
+
+nums = '8 9 4 1 2 4 7 5 7 5 7 1 4 4'
+miLista2 = list(nums.split())
+print(miLista2)
+
+print('______________________________________________')
+#range
+
+li = list(range(20))
+li1 = list(range(10,20))
+li2 = list(range(10,20,3))
+print(li)
+print(li1)
+print(li2)
+
+print('______________________________________________')
+
+# max devuelve el maximo de un conjunto de datos. 
+
+listaNum = [5,4,9,8,4,5,67,4,5,6,55,5,5,4,8,68,84]
+print(max(listaNum))
+
+# min devuelve el minimo de un conjunto de datos. 
+
+print(min(listaNum))
+
+#suma de todos los elementos: 
+
+print(sum(listaNum))
+
+
+print('______________________________________________')
+# promedio de datos de un conjunto de datos
+import statistics
+
+promedio = statistics.mean(listaNum)
+print(promedio)
+# El que mas se repite mode: 
+print(statistics.mode(listaNum))
+
+print('______________________________________________')
+# Matematica: 
+import math
+print(math.pi) # pi
+print(math.sin(45)) # seno
+print(math.pow(2,2)) # potencia
+print(math.factorial(4)) # factorial
+print(math.sqrt(25)) # raiz cuadrada
+print(math.trunc(45.99999999)) # truncar decimales, se los quita
+print(round(45.99999999)) # redondea
+
+print('______________________________________________')
+# aleatorios
+import random
+print(random.randint(10,20)) # numero aleatorio entre el rango dado
+print(random.random()) # numeros aleatorios entre 0 y 1
+print(random.randbytes(5)) 
+
+
 
