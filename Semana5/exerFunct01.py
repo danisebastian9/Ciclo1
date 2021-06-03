31d4e47ae2973b3900ae1156f22a276a17ae76b1
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-06-02 22:29:54 -0500

    Exerbasics

--- Semana5/exerFunct01.py
+++ Semana5/exerFunct01.py
@@ -1,3 +1,13 @@
 # 1.Crear una funcion que sirva para la vida cotidiana
 # 2.Buscar 2 funciones internas y mostrarlas en clase
 
+quanLlantas = int.upper(input('Ingrese cantidad de llantas a ordenar: '))
+
+def funcLlantas(quanLlantas):
+    if  quanLlantas > 1 and quanLlantas < 5: 
+        totalVenta = quanLlantas * 300
+    elif quanLlantas >= 5 and quanLlantas <= 10:
+        totalVenta = quanLlantas * 250
+    else: 
+        totalVenta = quanLlantas * 200
+    print(totalVenta)
