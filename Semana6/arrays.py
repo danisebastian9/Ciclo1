1798e81dd80d47cd9d00180674a01216eb4afe8d
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-06-08 16:26:29 -0500

    Arrays class

--- Semana6/arrays.py
+++ Semana6/arrays.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 arr = np.array([8,'9',4,1,2,3])
+print(arr[:3])
 print(arr)
 arr_nuevo = arr.astype(int)
 print(arr_nuevo)
@@ -10,3 +11,16 @@ print(array1)
 
 array2 = np.ones(10,dtype=int)
 print(array2)
+
+array3 = np.arange(1,11)
+print(array3)
+
+array4 = np.arange(1,21,2)
+print(array4)
+
+
+n = int(input('Digite tamaño del arreglo: -> '))
+arrayfor = np.zeros(n, dtype=int)
+for i in range(n):
+    arrayfor[i] = input('Digite un numero -> ')
+print(arrayfor)
