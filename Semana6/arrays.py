f70c18bf2a6bcef3da97270000dea07d3cc2b6c1
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-06-08 16:10:58 -0500

    arrays

--- Semana6/arrays.py
+++ Semana6/arrays.py
@@ -1,2 +1,12 @@
 import numpy as np
 
+arr = np.array([8,'9',4,1,2,3])
+print(arr)
+arr_nuevo = arr.astype(int)
+print(arr_nuevo)
+
+array1 = np.zeros(10,dtype=int)
+print(array1)
+
+array2 = np.ones(10,dtype=int)
+print(array2)
