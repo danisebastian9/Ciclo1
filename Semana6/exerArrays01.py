71bc292e3ad68c0f7483fb01227c618e4687bfc9
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-06-09 18:04:32 -0500

    import and input added

--- Semana6/exerArrays01.py
+++ Semana6/exerArrays01.py
@@ -3,3 +3,8 @@ Realicen en Python un programa que lea 10 números por teclado,
 los almacene en un arreglo y muestre la suma, 
 resta, multiplicación y división de todos.
 '''
+import numpy as np
+
+nums = int(input('Ingrese el numero a guardar -> '))
+
+
