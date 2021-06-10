0341e91937bd943fbd58a7deeaa91d5ae1065317
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-06-10 16:12:00 -0500

    New class exercise

---
+++ Semana6/arrays01.py
@@ -0,0 +1,10 @@
+#Leer una matriz de 5x5 y calcular la suma de cada una de sus filas y columnas, dejando dichos 
+#resultados en dos vectores, uno de la suma de las filas y otro de la suma de las columnas
+
+import numpy as np
+
+matriz = np.zeros((5,5), dtype=int)
+
+sumFilas = np.zero(5, dtype=int)
+sumColumn = np.zeros(5, dtype=int)
+
