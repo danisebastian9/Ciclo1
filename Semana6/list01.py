c9c0bab970740629c4163e20bf45218c427e8901
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-06-10 17:36:45 -0500

    lists completed

--- Semana6/list01.py
+++ Semana6/list01.py
@@ -25,7 +25,16 @@ print(lista2)
 
 numeros = input('Digite los numeros separados por espacio -> ')
 lista3 = numeros.split()
+lista3 = [int(i) for i in lista3]
 print(lista3)
+
+lista3.extend(lista2) # lista3.extend(range(20,30)) 
+print(lista3)
+
+print(lista3.count(2)) #Cuenta cuantas veces esta ese valor en la lista
+
+lista3.pop() # elimina el ultimo elemento. 
+lista3.pop(0) # busca el elemento en esa posicion y la elimina
 
 
 vocales = ['a','e','i','o','u']
