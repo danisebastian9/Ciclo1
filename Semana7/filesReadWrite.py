afed21354a398f5e560ef3f0d35e90bb04a927b1
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-06-15 16:42:45 -0500

    read and write added

--- Semana7/filesReadWrite.py
+++ Semana7/filesReadWrite.py
@@ -1,8 +1,8 @@
-from io import open # No es necesario importarlo. 
+# Lectura y escritura 'r+'
 
-# Lectura de archivos 'r' - 
+from io import open 
 
-archivo1 = open('archivos/archivo1.txt', 'r')
+archivo1 = open('archivos/archivo1.txt', 'r+')
 
 contenido = archivo1.read()
 print(contenido)
@@ -12,8 +12,12 @@ print(archivo1.readline())  # Leer linea por linea en la posicion actual.
 print(archivo1.readline())  # Leer linea por linea en la posicion actual.
 
 archivo1.seek(0)
-lista = archivo1.readlines()
+lista = archivo1.readlines() # convertir en una lista
 print(lista)
+
+numeros = [4,8,9,4,2,6,8]
+for i in numeros:
+    archivo1.write(str(i) + '\n')
 
 archivo1.close()
 
